aedea00cae184ccf3531f9a230cebb07fce72a2e
hbateman <hugobateman@gmail.com>
2014-08-17 11:35:38 +1200

    Removed a heap of un-needed functions, can now run a process in the background, added a few tests

--- PyCommandLine.py
+++ PyCommandLine.py
@@ -5,6 +5,7 @@ class CmdInterpreter():
 
 	hist = []
 	directory = ""
+	processes = []
 
 	def __init__(self):
 		 """retrieve current directory upon startup of shell"""
@@ -12,25 +13,28 @@ class CmdInterpreter():
 
 	"""Interpret a line of code, decide how to execute"""
 	def interpret(self, line):
+		amper = False
 		self.hist.append(line)
+		if (line[len(line)-1] == "&"):
+			amper = True
+			line = line.replace('&', '')
 		commands = line.split('|')
 		if not line:
 			output = ""
 		elif (self.checkPipes(commands)):
 			print("Invalid use of pipe '|'.")
 			return
-		elif (len(commands) == 1):
-			pid = os.fork()
-			if (pid == 0):
-				self.executeCommand(self.parseCommand(commands[0]))
-			else:
-				os.waitpid(pid, 0)
 		else:
 			pid = os.fork()
 			if (pid == 0):
 				self.execute(commands)
 			else:
-				os.waitpid(pid, 0)
+				if (amper == True):
+					self.processes.append(pid)
+					print("[" + str(self.processes.index(pid)+1) + "]" + " " + str(pid))
+					return
+				else:
+					os.waitpid(pid, 0)
 		return
 
 	"""Check valid use of pipes"""
@@ -50,28 +54,14 @@ class CmdInterpreter():
 
 	"""detirmine which command"""
 	def executeCommand(self, command):
-		if(command[0] == "echo"):
-			output = self.executeEcho(command)
-		elif(command[0] == "pwd"):
-			output = self.executePwd(command)
-		elif(command[0] == "cd"):
+		if(command[0] == "cd"):
 			self.executeCd(command)
-		elif(command[0] == "ls"):
-			self.executeLs(command)
-		elif(command[0] == "ps"):
-			self.executePs(command)
-		elif(command[0] == "wc"):
-			self.executeWc(command)
-		elif(command[0] == "grep"):
-			self.executeGrep(command)
-		elif(command[0] == "diff"):
-			self.executeDiff(command)		
 		elif(command[0] == "history"):
 			self.executeHistory(command)		
 		elif(command[0] == "h"):
 			self.executeHistory(command)
 		else:
-			print("command not found")
+			self.executeNorm(command)
 		return
 
 	"""Recursive function for case whe npipes are used"""
@@ -98,11 +88,8 @@ class CmdInterpreter():
 			command = commands[0]
 			self.executeCommand(self.parseCommand(command))
 
-	def executeEcho(self, arg):
-		os.execvp('/bin/echo', arg)
-
-	def executePwd(self, arg):
-		os.execvp('/bin/pwd', arg)
+	def executeNorm(self, arg):
+		os.execvp(arg[0], arg)
 
 	def executeCd(self, arg):
 		if (len(arg) == 1):
@@ -110,24 +97,8 @@ class CmdInterpreter():
 		else:
 			try:
 				os.chdir(arg[1])
-			except OSError as e:
-				print("cd: '", arg[1], "': No such file or directory", sep = '')
-				"""print(e.args)"""
-
-	def executeLs(self, arg):
-		os.execvp('/bin/ls', arg)
-
-	def executePs(self, arg):
-		os.execvp('/bin/ps', arg)
-
-	def executeWc(self, arg):
-		os.execvp('/usr/bin/wc', arg)
-
-	def executeDiff(self, arg):
-		os.execvp('/usr/bin/diff', arg)
-
-	def executeGrep(self, arg):
-		os.execvp('/bin/grep', arg)
+			except OSError:
+				print("cd: '", arg[1], "': No such file or directory", sep = "")
 
 	def executeHistory(self, arg):
 		"""If re-executing historic command
